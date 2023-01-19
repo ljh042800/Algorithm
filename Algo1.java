8f1cfb36f1eaa5c7e9b5f29d82ba1ff4b4070f98
이진혁 <dldlwls12346@naver.com>
2023-01-20 00:19:41 +0900

    1단계마지막

--- Algo1.java
+++ Algo1.java
@@ -5,11 +5,107 @@ public class Algo1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+//
+//        더하기
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a+b);
+//        빼기
+//
+//        int c = sc.nextInt();
+//        int d = sc.nextInt();
+//        System.out.println(c-d);
+//
+//        곱하기
+//
+//        int e = sc.nextInt();
+//        int f = sc.nextInt();
+//        System.out.println(e*f);
+//
+//        나누기
+//
+//        double g = sc.nextDouble();
+//        double h = sc.nextDouble();
+//        System.out.println(g/h);
+//
+        //사칙연산
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+//        System.out.println(a%b);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
+//        String a = sc.nextLine();
+//        System.out.println(a+"??!");
+
+//        int a = sc.nextInt();
+//        System.out.println(a-543);
+
+        //체스판
+//       int king  = 1;
+//       int queen = 1;
+//       int rook = 2;
+//       int bishop = 2;
+//       int knight = 2;
+//       int pawn = 8;
+//
+//       king = king-sc.nextInt();
+//       queen = queen-sc.nextInt();
+//       rook = rook-sc.nextInt();
+//       bishop = bishop-sc.nextInt();
+//       knight = knight-sc.nextInt();
+//       pawn = pawn-sc.nextInt();
+//       //출력값에 공백이 들어감.
+//        System.out.print(king+" ");
+//        System.out.print(queen+" ");
+//        System.out.print(rook+" ");
+//        System.out.print(bishop+" ");
+//        System.out.print(knight+" ");
+//        System.out.print(pawn);
+
+
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//
+//        System.out.println((a+b)%c);
+//        System.out.println((a%c)+(b%c)%c);
+//        System.out.println((a*b)%c);
+//        System.out.println((a%c)*(b%c)%c);
+
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        //472x385에서 5를 곱해야 2360이 나오는데, 여기서 5를 나오게해야함.
+//        System.out.println(a*(b%10));
+//        //8을 나오게해야함.
+//        System.out.println((b%100/10)*a);
+//        //3을 나오게해야함
+//        System.out.println(a*(b/100));
+//
+//        System.out.println(a*b);
+//       "\\" 가 \ 하나출력
+//        System.out.println("\\     /\\");
+//        System.out.println(")     ( ')");
+//        System.out.println("(   /  )");
+//        System.out.println("\\ (__) |");
+
+
+//        System.out.println("|\\_/|");
+//        System.out.println("|q p|    /}");
+//        System.out.println("( 0 ) \"\"\"\\");
+//        System.out.println("|\"^\"'     | ");
+//        System.out.println("||_/=\\__|");
+
+
+
+
+
 
 
     }
+
+
+
 }
